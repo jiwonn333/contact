ceb0a6188b22f06da9ed5930ebec54cfc9c250ae
happy jiwon <sjwon113@naver.com>
2024-04-07 20:27:11 +0900

    0. Scaffold() 상, 중, 하 로 나눠주는 Scaffold() 위젯 기본 사용법 아이콘 가로, 세로, 정렬 해보기

--- lib/main.dart
+++ lib/main.dart
@@ -8,21 +8,50 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // @override
+    // @override
   // Widget build(BuildContext context) {
   //   return MaterialApp(
   //       home: Center(
   //           child: Container(width: 30, height: 30, color: Colors.pink)));
   // }
 
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //       home: Scaffold(
+  //         appBar: AppBar(),
+  //         body: Container(),
+  //         bottomNavigationBar: BottomAppBar(child: Text('bottomNavigationBar Test Text'),),
+  //       )
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(),
-          body: Container(),
-          bottomNavigationBar: BottomAppBar(child: Text('bottomNavigationBar Test Text'),),
-        )
-    );
+            /** 1. 가로 배치 _ Row-children **/
+            // body: Row(
+            //   children: const [
+            //     Icon(Icons.star),
+            //     Icon(Icons.star),
+            //     Icon(Icons.star)])
+
+            /** 2. 세로 배치 _ Column-children **/
+            // body: Column(
+            //     children: const [
+            //         Icon(Icons.star),
+            //         Icon(Icons.star),
+            //         Icon(Icons.star)])
+
+            /** 3. Row, Column 요소 정렬 _ mainAxisAlignment **/
+            body: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+                    Icon(Icons.star),
+                    Icon(Icons.star),
+                    Icon(Icons.star)])
+        ));
   }
 }
