7fcbe47d0c41e4d08094a56504d54136efa325a1
happy jiwon <sjwon113@naver.com>
2024-04-08 15:47:00 +0900

    0. Button TextButton, ElevatedButton, IconButton 사용

--- lib/main.dart
+++ lib/main.dart
@@ -8,50 +8,74 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-    // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //       home: Center(
-  //           child: Container(width: 30, height: 30, color: Colors.pink)));
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //       home: Scaffold(
-  //         appBar: AppBar(),
-  //         body: Container(),
-  //         bottomNavigationBar: BottomAppBar(child: Text('bottomNavigationBar Test Text'),),
-  //       )
-  //   );
-  // }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            /** 1. 가로 배치 _ Row-children **/
-            // body: Row(
-            //   children: const [
-            //     Icon(Icons.star),
-            //     Icon(Icons.star),
-            //     Icon(Icons.star)])
+          appBar: AppBar(title: Text('이곳은 앱바')),
+          body: SizedBox(
+            /** 1. TextButton - 텍스트 형태의 버튼 **/
+            // child: TextButton(
+            //   child:Text('버튼안에 들어갈 글자'),onPressed: (){},)
 
-            /** 2. 세로 배치 _ Column-children **/
-            // body: Column(
-            //     children: const [
-            //         Icon(Icons.star),
-            //         Icon(Icons.star),
-            //         Icon(Icons.star)])
+            /** 2. ElevatedButton - 색상이 들어간 형태의 버튼 **/
+            // child: ElevatedButton(
+            //   child: Text("버튼입니당"), onPressed: (){},),
 
-            /** 3. Row, Column 요소 정렬 _ mainAxisAlignment **/
-            body: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: const [
-                    Icon(Icons.star),
-                    Icon(Icons.star),
-                    Icon(Icons.star)])
-        ));
+            /** 3. IconButton - 아이콘 버튼 **/
+            child: IconButton(
+              icon: Icon(Icons.phone), onPressed: (){},
+            ),
+
+            ),
+          )
+        );
   }
+
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//       home: Center(
+//           child: Container(width: 30, height: 30, color: Colors.pink)));
+// }
+
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(),
+//         body: Container(),
+//         bottomNavigationBar: BottomAppBar(child: Text('bottomNavigationBar Test Text'),),
+//       )
+//   );
+// }
+
+// @override
+// Widget build(BuildContext context) {
+//   return MaterialApp(
+//       home: Scaffold(
+//           /** 1. 가로 배치 _ Row-children **/
+//           // body: Row(
+//           //   children: const [
+//           //     Icon(Icons.star),
+//           //     Icon(Icons.star),
+//           //     Icon(Icons.star)])
+//
+//           /** 2. 세로 배치 _ Column-children **/
+//           // body: Column(
+//           //     children: const [
+//           //         Icon(Icons.star),
+//           //         Icon(Icons.star),
+//           //         Icon(Icons.star)])
+//
+//           /** 3. Row, Column 요소 정렬 _ mainAxisAlignment **/
+//           body: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               crossAxisAlignment: CrossAxisAlignment.center,
+//               children: const [
+//                   Icon(Icons.star),
+//                   Icon(Icons.star),
+//                   Icon(Icons.star)])
+//       ));
+// }
 }
